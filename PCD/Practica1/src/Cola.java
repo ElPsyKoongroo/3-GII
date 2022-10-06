e76f051256c39846716e10e7400f7ccf3ed68e50
Adrian <arrodriguez1809@gmail.com>
2022-10-06 23:40:00 +0200

    [PCD] Practica_1 Terminada

---
+++ PCD/Practica1/src/Cola.java
@@ -0,0 +1,59 @@
+/**
+ * @author ElPsy
+ */
+import java.util.*;
+
+
+public class Cola implements ICola
+{
+    private Object[] datos;
+    private int head;
+    private int tail;
+    private int capacidad;
+    private int numelementos;
+
+    public Cola(int capacidad) {
+
+        datos = new Object[capacidad];
+        head = 0;
+        tail = 0;
+        numelementos = 0;
+        this.capacidad = capacidad;
+    }
+
+    @Override
+    public int GetNum() { return numelementos; }
+
+    @Override
+    public void Acola(Object elemento) throws Exception {
+
+        if(colallena()) throw new Exception("La cola está llena");
+
+        datos[(tail++) % capacidad] = elemento;
+        numelementos++;
+
+    }
+
+    @Override
+    public Object Desacola() throws Exception {
+
+        if(colavacia()) throw new Exception("La cola está vacia");
+
+        numelementos--;
+        return datos[(head++) % capacidad];
+
+    }
+
+    @Override
+    public Object Primero() throws Exception{
+
+        if (colavacia()) throw new Exception("La cola está vacia");
+
+        return datos[head];
+    }
+
+    private boolean colavacia() { return numelementos == 0; }
+
+    private boolean colallena() { return numelementos == capacidad; }
+
+}
---
+++ PCD/Practica1/src/Main.java
@@ -0,0 +1,37 @@
+/**
+ * @author ElPsy
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        Cola cola = new Cola(10);
+
+        try{
+            for (int i = 0; i < 10; i++){
+                cola.Acola(i);
+                System.out.println("Metido " + i);
+            }
+        }
+        catch (Exception e){
+            System.out.println("Error al introducir elemento: " + e.getMessage());
+        }
+
+        try{
+            for (int i = 0; i < 9; i++){
+                int num = (int)cola.Desacola();
+                System.out.println("Eliminado elemento:" + num);
+            }
+        }
+        catch (Exception e){
+            System.out.println("Error al eliminar elemento: " + e.getMessage());
+        }
+
+        try {
+            int num = (int) cola.Primero();
+            System.out.println("Numero: " + num);
+        }
+        catch (Exception e){
+            System.out.println("Error al leer primer elemento: " + e.getMessage());
+        }
+    }
+}
