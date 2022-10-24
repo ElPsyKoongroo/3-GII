8a78293348aae5e4ec42abd9c79f16045295bae4
Adrian <arrodriguez1809@gmail.com>
2022-10-24 15:54:55 +0200

    a

--- AMC/Pipo/Pipo/Pipo.cpp
+++ AMC/Pipo/Pipo/Pipo.cpp
@@ -1,6 +1,6 @@
 #include "algoritmo.cpp"
 #define SIZE 20'000
-
+#define MEDIA 50
 
 
 
@@ -8,25 +8,26 @@ int main()
 {
     std::cout << "Hello World!\n";
     srand(time(NULL));
-    Punto* p = Algoritmo::GeneraPuntos(SIZE, 0, 100);
 
-    auto start = std::chrono::high_resolution_clock::now();
+    uint64_t med = 0;
+    for(int i = 0 ; i < MEDIA ; ++i){
+        Punto* p = Algoritmo::GeneraPuntos(SIZE, 0, 100);
 
+        auto start = std::chrono::high_resolution_clock::now();
 
-    Punto* mejores = Algoritmo::DivideVenceras(p);
+        Punto* mejores = Algoritmo::DivideVenceras(p);
 
-    auto finish = std::chrono::high_resolution_clock::now();
+        auto finish = std::chrono::high_resolution_clock::now();
 
-    auto time = std::chrono::duration_cast<std::chrono::milliseconds>(finish-start).count();
+        auto time = std::chrono::duration_cast<std::chrono::milliseconds>(finish-start).count();
 
+        med += time;
+        delete p;
 
-
-
-    for (int i = 0; i < 3; ++i)
-    {
-        std::cout << "x:" << mejores[i].x << ", y:" << mejores[i].y << std::endl;
     }
 
-    std::cout << time << "ms\n";
-    delete p;
+    med/=MEDIA;
+
+    std::cout << med << "ms\n";
+
 }
